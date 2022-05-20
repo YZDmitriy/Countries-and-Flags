61f5a5509e959b9be9e5afb428c27853aab43cac
Dmitriy <dyuzef@mail.ru>
2022-05-20 19:34:09 +0300

    add selected continent

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,13 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { CustomSelect } from './CustomSelect';
 import Search from './Search';
 
-function Controls(props) {
+const options = [
+  { value: 'Africa', label: 'Africa' },
+  { value: 'America', label: 'America' },
+  { value: 'Asia', label: 'Asia' },
+  { value: 'Europe', label: 'Europe' },
+  { value: 'Oceania', label: 'Oceania' },
+];
 
-  const [search, setSearch] = useState('')
+function Controls(props) {
+  const [search, setSearch] = useState('');
 
   return (
     <div>
-      <Search search={search} setSearch={setSearch}/>
+      <Search search={search} setSearch={setSearch} />
+      <CustomSelect options={options}/>
     </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import { IoSearch } from 'react-icons/io5';
 
-const InputContaier = styled.ladel`
+const InputContainer = styled.label`
   background-color: var(--colors-ui-base);
   padding: 1rem 2rem;
   display: flex;
@@ -11,7 +11,7 @@ const InputContaier = styled.ladel`
   border-radius: var(--radii);
   box-shadow: var(--shadow);
   width: 100%;
-  margin-bottom: 1.5rem;
+  margin-bottom: 1rem;
 
   @media (min-width: 767px) {
     margin-bottom: 0;
@@ -21,22 +21,23 @@ const InputContaier = styled.ladel`
 
 const Input = styled.input.attrs({
   type: 'search',
-  placeholder: 'Search for a country'
+  placeholder: 'Search for a country',
 })`
-margin-left: 2rem;
-border: none;
-outline: none;
-background-color: var(--colors-bg);
-color: var(--color-text);
+  margin-left: 2rem;
+  border: none;
+  outline: none;
+  color: var(--color-text);
+  background-color: var(--colors-ui-base);
 `;
 
-function Search({ search, setSearch }) {
+const  Search =({ search, setSearch }) => {
   return (
-    <InputContaier>
+    <InputContainer>
       <IoSearch />
       <Input onChange={(e) => setSearch(e.target.value)} value={search} />
-    </InputContaier>
+    </InputContainer>
   );
 }
 
 export default Search;
+
