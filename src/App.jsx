d23375f79bb301863787c482ed2aeea59ddfa85b
Dmitriy <dyuzef@mail.ru>
2022-05-22 10:25:25 +0300

    add card countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,52 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Card from './components/Card';
 
 import Controls from './components/Controls';
 import Header from './components/Header';
+import List from './components/List';
 import Main from './components/Main';
 import { ALL_COUNTRIES } from './config';
 
 function App() {
   const [countries, setCountries] = useState([]);
 
-  console.log(countries)
+  console.log(countries);
   useEffect(() => {
-    axios.get(ALL_COUNTRIES).then(
-      ({data}) => setCountries(data))
-    )
+    axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
   }, []);
 
   return (
     <>
       <Header />
-
       <Main>
         <Controls />
+        <List>
+          {countries.map((c) => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: 'Population',
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: 'Region',
+                  description: c.region,
+                },
+                {
+                  title: 'Capital',
+                  description: c.capital,
+                },
+              ],
+            };
+            // console.log(countryInfo);
+            return (
+              <Card key={c.name} {...countryInfo}/>
+            )
+          })}
+        </List>
       </Main>
     </>
   );
