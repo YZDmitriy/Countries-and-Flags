d0e2a4fae942f57c77b0e6957a66437f88bf2bbe
Dmitriy <dyuzef@mail.ru>
2022-05-22 17:51:26 +0300

    add card country and about country

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { IoArrowBack } from 'react-icons/io5';
+import { searchByCountry } from '../config';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
 
-function Details({match}) {
+function Details() {
+  const { name } = useParams();
+  const navigate = useNavigate();
+
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
+
   return (
     <div>
-      werty
+      <Button onClick={() => navigate(-1)}>
+        <IoArrowBack /> Back
+      </Button>
+      {country && <Info {...country} />}
     </div>
   );
 }
